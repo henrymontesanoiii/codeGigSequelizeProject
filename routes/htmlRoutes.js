e8fda789d4aa3edee7b8d23141f5681f4127ad92
henman3 <henrymontesanoiii@gmail.com>
2019-08-10 12:44:27 -0400

    Added insert into database functionality with placeholder data before form addition

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,39 @@
 var Gig = require("../models/gig");
 module.exports = function(app) {
-  // Load index page
-  app.get('/', function(req, res) {
-    console.log('Home');
-    res.send('Home');
-    
-  });
+  //Get all gigs and return as JSON
   app.get("/gigs", function(req, res) {
     Gig.findAll({}).then(function(results) {
-
+      console.log(results);
       res.json(results);
+    })
+    .catch(err => console.log("Error:" + err));;
+  });
+
+  //Add gig to database
+  app.get("/add", function(req, res) {
+    const data = {
+      title:' Sql Developer',
+      technologies: 'Sql',
+      budget: 50000,
+      description: 'Knowledge of Sql',
+      contact_email: 'jobs@sql.com'
+    };
+    let {title, technologies, budget, description, contact_email} = data;
+    Gig.create({
+      title: title,
+      technologies: technologies,
+      budget: budget,
+      description: description,
+      contact_email: contact_email
+    }).then(function(newGig) {
+      console.log(newGig);
+      res.redirect('gigs');
     })
     .catch(err => console.log("Error:" + err));;
   });
  
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.send("404");
   });
 };
